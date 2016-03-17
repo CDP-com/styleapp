b5621529e35754337ab2dc8fcd8314ca129d2ac8
smmadara <smmadara@gmail.com>
2016-03-17 13:28:13 -0400

    Initial Commit
    Checked in the first version of The Style App.

---
+++ app-lib.js
@@ -0,0 +1,23 @@
+//***** Use this file to set up your buttons and include any custom functions for your app *****
+// 
+var development = 1;                   // 1 this app is in development, 0 this app is in production
+var appname = "styleapp";           // This apps name / folder name _ must be unique
+var btncount = 0;                      // This is the number of buttons on the home page of the app
+
+//var currentUserDefaultCaution = "0";    // **This is the global Throw caution to the wind flag for the current use
+
+/*-----------------------------------------------------*/
+/*---------------------- Buttons ----------------------*/
+/*-----------------------------------------------------*/
+
+//No Buttons for this App
+	
+/*-----------------------------------------------------*/
+/*----------------- Custom Functions ------------------*/
+/*-----------------------------------------------------*/	
+//***** Use the below area for your common functions *****
+
+
+
+
+
